b21058c0605fd768ab7fce0cab02bc8763bce6a4
jipan <pan.ji@itaotuo.com>
2016-01-10 17:42:11 +0800

    add multi sheets supports

--- test/main.js
+++ test/main.js
@@ -2,27 +2,67 @@
 var should = require('should');
 var nodeExcel = require('../index');
 
-
 describe('Simple Excel xlsx Export', function() {
-    describe('Export', function() {
-        it('returns xlsx', function() {
-		var conf ={};
-		conf.cols = [
-			{caption:'string', type:'string'},
-			{caption:'date', type:'date'},
-			{caption:'bool', type:'bool'},
-			{caption:'number 2', type:'number'}				
-		];
-		conf.rows = [
-			['pi', (new Date(Date.UTC(2013, 4, 1))).oaDate(), true, 3.14],
-			["e", (new Date(2012, 4, 1)).oaDate(), false, 2.7182],
-			["M&M<>'", (new Date(Date.UTC(2013, 6, 9))).oaDate(), false, 1.2],
-			["null", null, null, null]
-		];
-			
-	      var result = nodeExcel.execute(conf),
-				    fs = require('fs');
-				fs.writeFileSync('d.xlsx', result, 'binary');			
+	describe('Export', function() {
+		it('returns xlsx', function() {
+			var conf = {};
+			conf.name = 'xxxxxx';
+			conf.cols = [{
+				caption: 'string',
+				type: 'string'
+			},
+			{
+				caption: 'date',
+				type: 'date'
+			},
+			{
+				caption: 'bool',
+				type: 'bool'
+			},
+			{
+				caption: 'number 2',
+				type: 'number'
+			}];
+			conf.rows = [['pi', (new Date(Date.UTC(2013, 4, 1))).oaDate(), true, 3.14], ["e", (new Date(2012, 4, 1)).oaDate(), false, 2.7182], ["M&M<>'", (new Date(Date.UTC(2013, 6, 9))).oaDate(), false, 1.2], ["null", null, null, null]];
+
+			var result = nodeExcel.execute(conf);
+			//console.log(result);
+
+			var fs = require('fs');
+			fs.writeFileSync('d.xlsx', result, 'binary');
 		});
-    });
+		it('returns multisheet xlsx', function() {
+			var confs = [];
+			var conf = {};
+			conf.name = 'abcsheetName';
+			conf.cols = [{
+				caption: 'string',
+				type: 'string'
+			},
+			{
+				caption: 'date',
+				type: 'date'
+			},
+			{
+				caption: 'bool',
+				type: 'bool'
+			},
+			{
+				caption: 'number 2',
+				type: 'number'
+			}];
+			conf.rows = [['hahai', (new Date(Date.UTC(2013, 4, 1))).oaDate(), true, 3.14], ["e", (new Date(2012, 4, 1)).oaDate(), false, 2.7182], ["M&M<>'", (new Date(Date.UTC(2013, 6, 9))).oaDate(), false, 1.2], ["null", null, null, null]];
+			for (var i = 0; i < 3; i++) {
+				conf = JSON.parse(JSON.stringify(conf));
+				conf.name = 'sheet我要'+i;
+				confs.push(conf);
+			}
+			console.log(confs);
+
+			var result = nodeExcel.execute(confs),
+			fs = require('fs');
+			fs.writeFileSync('ds.xlsx', result, 'binary');
+		})
+	});
 });
+
